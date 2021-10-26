4e77b1de715b223506d58d4e05a86af5b9facbd5
Lars Andersson <laand19@student.sdu.dk>
2021-10-26 10:26:47 +0200

    Login component

---
+++ src/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="Login">
+    <p>{{"Username"}}</p>
+    <input v-model="usernameText" placeholder="username">
+    <p>{{"Password"}}</p>
+    <input v-model="passwordText" placeholder="password">
+    <p>{{""}}</p>
+    <button class="Button"><slot>Forgot Password</slot></button>
+    <div class="loginButton">
+      <button class="Button"><slot>Login</slot></button>
+    </div>
+
+     </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  props: {
+    usernameText: String,
+    passwordText: String
+  }
+}
+</script>
+
+<style scoped>
+.loginButton {
+  margin-left: 30%;
+}
+</style>
