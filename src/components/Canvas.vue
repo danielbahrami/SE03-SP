7c08185b12ef17fcf8a36314dbeb599c51352566
Janno <janpe20@student.sdu.dk>
2021-10-26 15:34:59 +0200

    Added Canvas
    The Canvas file also contains a header

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,29 +1,40 @@
 <template>
-<div class="topnav">
-  <img src="#">
-  <h1>Alrakis</h1>
-  <input type="text" placeholder="search...">
-</div>
+  <div class="topnav">
+    <img style="vertical-align: center" width="80" height="80"
+         src="https://cdn.discordapp.com/attachments/779250503741079563/902135385298190357/tetete-removebg-preview.png">
+    <span class="le-title">Alrakis</span>
+    <input type="text" placeholder="search...">
+  </div>
 </template>
 <script>
 export default {
   name: 'Canvas'
 }
 </script>
+
 <style scoped>
-.topnav{
+.topnav {
   overflow: hidden;
   background: #3F3F3F;
 }
-.topnav input[type=text]{
+
+.le-title {
+  font-family: Arial;
+  font-size: 80px;
+}
+
+.topnav input[type=text] {
   float: right;
-  padding: 6px;
+  padding: 7px;
   border: none;
   margin-top: 8px;
   margin-right: 16px;
   font-size: 17px;
+  display: flex;
+  align-items: center;
 }
-body{
+
+body {
   background: #1E1E1E;
 }
 </style>
