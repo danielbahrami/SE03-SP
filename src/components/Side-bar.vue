f5f1787b7447a3ed82e18d482cdc27153165e413
Ashvikan Sivabalaa <ashvikan10@gmail.com>
2021-10-27 10:01:05 +0200

    Sidebar

--- src/components/Side-bar.vue
+++ src/components/Side-bar.vue
@@ -1,10 +1,89 @@
 <template>
-  $END$
+  <sidebar-menu :menu="menu" />
 </template>
 
 <script>
 export default {
-  name: "sidebar"
+  data() {
+    return {
+      menu: [
+        {
+          header: true,
+          title: 'Sidebar',
+          hiddenOnCollapse: true
+        },
+        {
+          href: '/',
+          title: 'Home',
+          icon: 'fa fa-user'
+        },
+        {
+          href: '/playlist',
+          title: 'Playlist',
+          icon: 'fa fa-chart-area',
+          child: [
+            {
+              href: '/playlist/sublink',
+              title: '#1'
+            },
+            {
+              href: '/playlist/sublink',
+              title: '#2'
+            }
+
+          ]
+        },
+        {
+          href: '/hitlist',
+          title: 'Hitlist',
+          icon: 'fa fa-chart-area',
+          child: [
+            {
+              href: '/hitlist/sublink',
+              title: '#1'
+            },
+            {
+              href: '/hitlist/sublink',
+              title: '#2'
+            }
+
+          ]
+        },
+        {
+          href: '/genre',
+          title: 'Genre',
+          icon: 'fa fa-chart-area',
+          child: [
+            {
+              href: '/genre/sublink',
+              title: '#1'
+            },
+            {
+              href: '/genre/sublink',
+              title: '#2'
+            }
+
+          ]
+        },
+        {
+          href: '/recently_played',
+          title: 'Recently Played',
+          icon: 'fa fa-chart-area',
+          child: [
+            {
+              href: '//recently_played/sublink',
+              title: '#1'
+            },
+            {
+              href: '//recently_played/sublink',
+              title: '#2'
+            }
+
+          ]
+        },
+      ]
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueSidebarMenu from 'vue-sidebar-menu'
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
+Vue.use(VueSidebarMenu)
 
 
 Vue.config.productionTip = false
