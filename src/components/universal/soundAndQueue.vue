35d9d580313977c31cb4d27a7f79be11fc3f60fe
Cazpar <Casti19@student.sdu.dk>
2021-11-10 21:08:45 -0800

    CSS for volume slider

--- src/components/universal/soundAndQueue.vue
+++ src/components/universal/soundAndQueue.vue
@@ -5,13 +5,14 @@
         <span v-else-if="value > 20 && value < 70"><VolumeMedium/></span>
         <span v-else-if="value > 70 && value < 101"><VolumeHigh/></span>
         <div class="sliderContainer">
-            <vue-slider v-model="value"></vue-slider>
+            <vue-slider v-model="value" class="my-vue-slider"></vue-slider>
         </div>
         <!-- TODO: IMPLEMENT SPEED CHOOSER -->
     </div>
 </template>
 <script>
-import VueSlider from 'vue-slider-component'
+import VueSlider from 'vue-slider-component/dist-css/vue-slider-component.umd.min.js'
+import 'vue-slider-component/dist-css/vue-slider-component.css'
 import 'vue-slider-component/theme/material.css'
 
 import VolumeMute from 'vue-material-design-icons/VolumeMute.vue'
@@ -36,7 +37,7 @@ export default {
 </script>
 <style scoped>
     .soundAndQueue{
-        background: darkslategrey;
+        background: rgb(111, 110, 107);
         width: 17%;
         height: 2em;
         position: fixed;
@@ -45,6 +46,7 @@ export default {
         justify-content: center;
         align-items: center;
         z-index: 999;
+        border-left: 0.125em solid black;
     }
     .sliderContainer{
         width: 40%;
@@ -58,6 +60,32 @@ export default {
         display: inline-block;
         position: absolute;
         top: 0.13em;
+    }
+
+    /* Little deep on the css tree but seemingly necessary to overwrite imported css in script */
+    /* Change the height of the volume bar */
+    div.my-vue-slider.vue-slider.vue-slider-ltr[data-v-7fe1e2f6] {
+        height: 2px !important;
+    } 
+    /* Change color of 'unused' volume bar */
+    div.my-vue-slider.vue-slider.vue-slider-ltr[data-v-7fe1e2f6] > :nth-child(1) {
+        background-color: darkgrey !important;
+    }
+    /* Change color of the progressed slider (how high the volume is) */
+    div.my-vue-slider.vue-slider.vue-slider-ltr[data-v-7fe1e2f6] > :nth-child(1) > :nth-child(1){
+        background-color: #D6CD81 !important;
+    }
+    /* Remove the display of the handler */
+    div.my-vue-slider.vue-slider.vue-slider-ltr[data-v-7fe1e2f6] > :nth-child(1) > :nth-child(2) > :nth-child(1){
+        display: none !important;
+    }
+    /* Remove the border of the handler which is done by pseudo element after */
+    div.my-vue-slider.vue-slider.vue-slider-ltr[data-v-7fe1e2f6] > :nth-child(1) > :nth-child(2) > :nth-child(1):after{
+        display: none !important;
+    }
+    /* Change the background of the popup tooltip when adjusting volume */
+    div.my-vue-slider.vue-slider.vue-slider-ltr[data-v-7fe1e2f6] > :nth-child(1) > :nth-child(2) > :nth-child(2) > :nth-child(1){
+        background-color: rgb(111, 110, 107) !important;
     }
 
 </style>
