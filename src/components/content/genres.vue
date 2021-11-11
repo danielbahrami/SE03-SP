4eb572998434023f82a290b36eb354027713d3f4
faezeh <rfaezeh2@gmail.com>
2021-11-11 09:59:19 +0100

    genres updated

--- src/components/content/genres.vue
+++ src/components/content/genres.vue
@@ -2,29 +2,39 @@
   <div class="genres">
     <h1 class="title">Genres</h1>
     <ul class="container">
-      <li><a href="#"> <img alt="" height="175" src="https://bit.ly/3pVbZCy" width="175"/>Genre 1</a></li>
+      <li><a href="#"> <img alt="" height="175" src="../../assets/Genres/Jazz.png" width="175"/>Genre 1</a></li>
 
-      <li><a href="#"> <img alt="" height="175" src="https://bit.ly/3vX55Oh" width="175"/>Genre 2</a></li>
+      <li><a href="#"> <img alt="" height="175"  src="../../assets/Genres/Rock.png" width="175"/>Genre 2</a></li>
 
-      <li> <a href="#"> <img alt="" height="175" src="https://bit.ly/3GtyoNm" width="175"/>Genre 3</a><li>
+      <li> <a href="#"> <img alt="" height="175" src="../../assets/Genres/Pop.png" width="175"/>Genre 3</a><li>
 
-      <li><img alt="" height="175" src="https://bit.ly/2ZtQVI2" width="175"/>Genre 4</li>
-      <li><img alt="" height="175" src="https://bit.ly/3nupEha" width="175"/>Genre 5</li>
-      <li><img alt="" height="175" src="https://bit.ly/3GtyoNm" width="175"/>Genre 6</li>
-      <li><img alt="" height="175" src="https://bit.ly/2ZtQVI2" width="175"/>Genre 7</li>
-      <li><img alt="" height="175" src="https://bit.ly/3nupEha" width="175"/>Genre 8</li>
-      <li><img alt="" height="175" src="https://bit.ly/3GtyoNm" width="175"/>Genre 9</li>
-      <li><img alt="" height="175" src="https://bit.ly/2ZtQVI2" width="175"/>Genre 10</li>
-      <li><img alt="" height="175" src="https://bit.ly/3nupEha" width="175"/>Genre 11</li>
-      <li><img alt="" height="175" src="https://bit.ly/3GtyoNm" width="175"/>Genre 12</li>
-      <li><img alt="" height="175" src="https://bit.ly/2ZtQVI2" width="175"/>Genre 13</li>
-      <li><img alt="" height="175" src="https://bit.ly/3nupEha" width="175"/>Genre 14</li>
-      <li><img alt="" height="175" src="https://bit.ly/3GtyoNm" width="175"/>Genre 15</li>
-      <li><img alt="" height="175" src="https://bit.ly/2ZtQVI2" width="175"/>Genre 16</li>
-      <li><img alt="" height="175" src="https://bit.ly/3nupEha" width="175"/>Genre 17</li>
-      <li><img alt="" height="175" src="https://bit.ly/3GtyoNm" width="175"/>Genre 18</li>
-      <li><img alt="" height="175" src="https://bit.ly/2ZtQVI2" width="175"/>Genre 19</li>
-      <li><img alt="" height="175" src="https://bit.ly/2ZtQVI2" width="175"/>Genre 20</li>
+      <li> <a href="#"> <img alt="" height="175" src="../../assets/Genres/Classic.png" width="175"/>Genre 4 </a></li>
+
+      <li> <a href="#"> <img alt="" height="175" src="../../assets/Genres/Country.png" width="175"/>Genre 5 </a></li>
+
+      <li> <a href="#"> <img alt="" height="175" src="../../assets/Genres/Blues.png" width="175"/>Genre 6</a></li>
+
+      <li> <a href="#"> <img alt="" height="175" src="../../assets/Genres/Soul.png" width="175"/>Genre 7</a></li>
+
+      <li> <a href="#"> <img alt="" height="175" src="../../assets/Genres/Dance.png" width="175"/>Genre 8</a></li>
+
+      <li> <a href="#"> <img alt="" height="175" src="../../assets/Genres/HipHop.png" width="175"/>Genre 9</a></li>
+
+      <li> <a href="#"> <img alt="" height="175" src="../../assets/Genres/Kpop.png" width="175"/>Genre 10</a></li>
+
+      <li> <a href="#"> <img alt="" height="175" src="../../assets/Genres/Rap.png" width="175"/>Genre 11</a></li>
+
+      <li> <a href="#"> <img alt="" height="175" src="../../assets/Genres/LiveMusic.png" width="175"/>Genre 12</a></li>
+
+      <li> <a href="#"> <img alt="" height="175" src="../../assets/Genres/RnB.png" width="175"/>Genre 13</a></li>
+
+      <li> <a href="#"> <img alt="" height="175" src="../../assets/Genres/Metal.png" width="175"/>Genre 14</a></li>
+
+      <li> <a href="#"> <img alt="" height="175" src="../../assets/Genres/Reggae.png" width="175"/>Genre 15</a></li>
+
+      <li> <a href="#"> <img alt="" height="175" src="../../assets/Genres/Folk.png" width="175"/>Genre 16</a></li>
+
+
     </ul>
     <br><br>
   </div>
