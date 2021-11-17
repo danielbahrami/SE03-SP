f0cdf9346ba1363c27acaccc304a4aafb5efbdc0
Daniel Bahrami <daniel@bahrami.dk>
2021-11-17 12:53:04 +0100

    Remove comments

--- src/components/universal/topbar.vue
+++ src/components/universal/topbar.vue
@@ -17,12 +17,9 @@
       </div>
     </div>
     <div class="div4 buttons">
-      <!-- Login button and that other weird button -->
-      <!-- <router-link active-class="active" class="top-btn" to="/profile"> -->
       <button aria-label="User" class="user__actions" @click="$vm2.open('login')">
         <PersonIcon/>
       </button>
-      <!-- </router-link> -->
       <button aria-label="har ingen ide hvad denne knap er til for" class="VIP__STAR">
         <img height="24px" src="../../assets/free-rating-star-icon-2793-thumb.png"/>
       </button>
