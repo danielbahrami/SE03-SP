19b27629ac00e0523a9ee02a4ba2334da72e8520
Daniel Bahrami <daniel@bahrami.dk>
2021-11-18 11:54:55 +0100

    Favourites component

---
+++ src/components/content/favorites.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="favorites">
+    <h1 class="header">Favorites</h1>
+    <table class="table">
+      <tr>
+        <th>Name</th>
+        <th>Artist</th>
+        <th>Album</th>
+      </tr>
+      <tr>
+        <td>My Universe</td>
+        <td>Coldplay</td>
+        <td>Music of Spheres</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "favorites"
+}
+
+</script>
+
+<style scoped>
+
+.favorites {
+  margin-left: 10em;
+}
+
+th {
+  border: 2px solid #FFFFFF;
+}
+
+td {
+  border: 2px solid #FFFFFF;
+}
+
+.header {
+  color: #D6CD81;
+}
+
+.table {
+  color: #FFFFFF;
+  text-align: left;
+  width: 100%;
+}
+
+</style>
