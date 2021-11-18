d97c4c1688e9812020928cb05b805fdbf4a4bf9d
Cazpar <Casti19@student.sdu.dk>
2021-11-18 03:04:16 -0800

    Fix table css

--- src/components/content/favorites.vue
+++ src/components/content/favorites.vue
@@ -1,28 +1,37 @@
 <template>
   <div class="favorites">
     <h1 class="header">Favorites</h1>
-    <table class="table">
-      <tr>
-        <th>Name</th>
-        <th>Artist</th>
-        <th>Album</th>
-      </tr>
-      <tr>
-        <td>My Universe</td>
-        <td>Coldplay</td>
-        <td>Music of Spheres</td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-    </table>
+    <table class="tg">
+<thead>
+  <tr>
+    <th class="tg-tizx">Name</th>
+    <th class="tg-tizx">Artist</th>
+    <th class="tg-tizx">Album</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td class="tg-tizx"></td>
+    <td class="tg-tizx"></td>
+    <td class="tg-tizx"></td>
+  </tr>
+  <tr>
+    <td class="tg-tizx"></td>
+    <td class="tg-tizx"></td>
+    <td class="tg-tizx"></td>
+  </tr>
+  <tr>
+    <td class="tg-tizx"></td>
+    <td class="tg-tizx"></td>
+    <td class="tg-tizx"></td>
+  </tr>
+  <tr>
+    <td class="tg-tizx"></td>
+    <td class="tg-tizx"></td>
+    <td class="tg-tizx"></td>
+  </tr>
+</tbody>
+</table>
   </div>
 </template>
 
@@ -37,25 +46,23 @@ export default {
 <style scoped>
 
 .favorites {
-  margin-left: 10em;
-}
-
-th {
-  border: 2px solid #FFFFFF;
-}
-
-td {
-  border: 2px solid #FFFFFF;
+  margin: 0 auto;
 }
 
 .header {
   color: #D6CD81;
 }
-
-.table {
-  color: #FFFFFF;
-  text-align: left;
+table{
   width: 100%;
 }
+
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg .tg-tizx{background-color:#000000;border-color:#ffffff;color:#ffffff;text-align:left;vertical-align:top}
+
+
 
 </style>
