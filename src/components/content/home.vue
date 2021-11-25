5edb426f68143c62b0553b0f39c7c4616b8924ca
Daniel Bahrami <daniel@bahrami.dk>
2021-11-25 10:52:01 +0100

    Remove bit.ly links

--- src/components/content/home.vue
+++ src/components/content/home.vue
@@ -2,15 +2,15 @@
   <div class="home">
     <h1 class="recommended">Recommended Playlists</h1>
     <ul class="container">
-      <li><img alt="" height="300" src="https://bit.ly/2ZtQVI2" width="300"/>Kap Kap<p class="Author">Jan Per </p></li>
-      <li><img alt="" height="300" src="https://bit.ly/3nupEha" width="300"/>Zu Zu<p class="Author">Jan Per </p></li>
-      <li><img alt="" height="300" src="https://bit.ly/3GtyoNm" width="300"/>Da Da<p class="Author">Jan Per </p></li>
+      <li><img alt="" height="300" src="https://exclaim.ca/images/justin-beiber-justice.jpg" width="300"/>Kap Kap<p class="Author">Jan Per </p></li>
+      <li><img alt="" height="300" src="https://1265745076.rsc.cdn77.org/1024/jpg/47466-travis-scott-astroworld-LP-5ba0adf4935c7.jpg" width="300"/>Zu Zu<p class="Author">Jan Per </p></li>
+      <li><img alt="" height="300" src="https://linkstorage.linkfire.com/medialinks/images/e28991cd-3750-4e87-9d6b-b360aecb6c67/artwork-440x440.jpg" width="300"/>Da Da<p class="Author">Jan Per </p></li>
     </ul>
     <h1 class="workout">Workout Playlists</h1>
     <ul class="container1">
-      <li><img alt="" height="300" src="https://bit.ly/3nDyjOi" width="300"/><br>Bum Bum<p class="Author">Jan Per </p></li>
-      <li><img alt="" height="300" src="https://bit.ly/3GmRWmi" width="300"/><br>Li Li<p class="Author">Jan Per </p></li>
-      <li><img alt="" height="300" src="https://bit.ly/3ml6Z7Y" width="300"/><br>Fr Fr<p class="Author">Jan Per </p></li>
+      <li><img alt="" height="300" src="https://graphicriver.img.customer.envatousercontent.com/files/322229646/archetypology-albumcover-template-preview.jpg?auto=compress,format&fit=crop&crop=top&w=590&h=590&s=f9568e3ec730fd8a51926762468c375d" width="300"/><br>Bum Bum<p class="Author">Jan Per </p></li>
+      <li><img alt="" height="300" src="https://upload.wikimedia.org/wikipedia/en/a/a0/Ariana_Grande_-_Positions.png" width="300"/><br>Li Li<p class="Author">Jan Per </p></li>
+      <li><img alt="" height="300" src="https://video-images.vice.com/_uncategorized/1527898271751-0bffd93463afe53e7f651f72bedfc78b1000x1000x1.jpeg" width="300"/><br>Fr Fr<p class="Author">Jan Per </p></li>
     </ul>
     <br><br>
   </div>
