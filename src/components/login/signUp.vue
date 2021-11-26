4649a365e8e55c2f1ccaefeb7408b46f689160d3
Daniel Bahrami <daniel@bahrami.dk>
2021-11-26 10:52:23 +0100

    Cleanup

--- src/components/login/signUp.vue
+++ src/components/login/signUp.vue
@@ -1,63 +1,73 @@
 <template>
   <modal-vue
-      @on-close="$vm2.close('signUp')"
-      name="signUp"
-      darkMode
-      fontDark
-      noFooter
-      noHeader
-      modalSize="md"
+    @on-close="$vm2.close('signUp')"
+    name="signUp"
+    darkMode
+    fontDark
+    noFooter
+    noHeader
+    modalSize="md"
   >
-
-                <div class="signUp" id="signUp">
-                  <div class="close">
-                    <exitIcon class="floatLeft" @click="close"/>
-                  </div>
-                  <div id="parent">
-                    <h1 class="h1">Registration</h1>
-                  </div>
-                  <div class="content">
-                    <label for="email">Email</label>
-                    <input type="email" class="input form-control" id="email" name="email" placeholder="Email">
-                  </div>
-                  <div class="content">
-                    <label for="password">Password</label>
-                    <input type="password" class="input form-control" id="password" name="password" placeholder="Password">
-                  </div>
-                  <button class="button">
-                    <slot>Sign Up</slot>
-                  </button>
-                  <br><br>
-                  <a class="login" @click="$vm2.open('login'), $vm2.close('signUp')">Login</a>
-                </div>
-            </modal-vue>
+    <div class="signUp" id="signUp">
+      <div class="close">
+        <exitIcon class="floatLeft" @click="close" />
+      </div>
+      <div id="parent">
+        <h1 class="h1">Registration</h1>
+      </div>
+      <div class="content">
+        <label for="email">Email</label>
+        <input
+          type="email"
+          class="input form-control"
+          id="email"
+          name="email"
+          placeholder="Email"
+        />
+      </div>
+      <div class="content">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          class="input form-control"
+          id="password"
+          name="password"
+          placeholder="Password"
+        />
+      </div>
+      <button class="button">
+        <slot>Sign Up</slot>
+      </button>
+      <br /><br />
+      <a class="login" @click="$vm2.open('login'), $vm2.close('signUp')"
+        >Login</a
+      >
+    </div>
+  </modal-vue>
 </template>
 
 <script>
-
-import exitIcon from 'vue-material-design-icons/Close.vue'
+import exitIcon from "vue-material-design-icons/Close.vue";
 
 export default {
-  name: 'signUp',
+  name: "signUp",
   components: {
-    exitIcon
+    exitIcon,
   },
   props: {
     emailText: String,
     newPasswordText: String,
-    newUsernameText: String
+    newUsernameText: String,
   },
   methods: {
     close() {
       this.$vm2.close("signUp");
-    }
-  }
-}
-
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 #signUp {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -80,7 +90,6 @@ export default {
 
 .h1 {
   color: rgb(212, 205, 138);
-
 }
 
 a.login {
@@ -93,19 +102,19 @@ a.login {
   padding-left: 10px;
 }
 
-.floatLeft{
+.floatLeft {
   float: left;
   color: white;
   cursor: pointer;
 }
 
-.floatLeft>svg{
+.floatLeft > svg {
   margin-top: 1.25rem;
   margin-left: 1.25rem;
   display: flex;
 }
 
-.content{
+.content {
   margin-top: 1rem;
   display: flex;
   align-content: space-around;
@@ -115,30 +124,30 @@ a.login {
 }
 
 .form-control {
-    align-self: center;
-    display: block;
-    width: 45%;
-    padding: 0.375rem 0.75rem;
-    font-size: 1rem;
-    line-height: 1.5;
-    color: #495057;
-    background-color: #fff;
-    background-clip: padding-box;
-    border: 1px solid #ced4da;
-    transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+  align-self: center;
+  display: block;
+  width: 45%;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  color: #495057;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid #ced4da;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
 }
 
 label {
-    display: inline-block;
-    margin-bottom: 0.5rem;
-    color: white;
+  display: inline-block;
+  margin-bottom: 0.5rem;
+  color: white;
 }
 
-button[data-v-a65090a4]{
+button[data-v-a65090a4] {
   margin-top: 1rem;
 }
 
-.vm2_wrapper.vm2__animate-opacity{
-    backdrop-filter: blur(5px);
+.vm2_wrapper.vm2__animate-opacity {
+  backdrop-filter: blur(5px);
 }
 </style>
